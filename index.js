4d7afd8ae151fc3f363c68f9441e3e6207d7906f
blumekonstantin11 <blumekonstantin11@gmail.com>
2025-10-02 19:24:27 +0200

    Fix: Port-Konfiguration für Render-Deployment

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const sqlite3 = require('sqlite3').verbose();
 const multer = require('multer'); // Multer-Paket importieren
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const options = {
     key: fs.readFileSync('key.pem'),
     cert: fs.readFileSync('cert.pem')
